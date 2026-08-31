dda49cf69e3aca62fd87a26a46c15886575d8ed3
ntp1212 <135628612+ntp1212@users.noreply.github.com>
2026-08-31 12:30:54 +0900

    Update Sec14_Ex6.js

--- Sec14/Sec14_Ex6/Sec14_Ex6.js
+++ Sec14/Sec14_Ex6/Sec14_Ex6.js
@@ -1,6 +1,6 @@
 const lists = [
     {
-        id: "1",
+        id: 1,
         bookName: "Abk",
         author : "ji",
         year : 2020,
@@ -17,10 +17,10 @@ let input = Number(prompt(`
     Nhập lựa chọn của bạn:`));
 function bookLists() {
     for (let i in lists) {
-        console.log("Id: ", lists[index].id);
-        console.log("Ten sach: ", lists[index].bookName);
-        console.log("Tac gia: ", lists[index].author);
-        console.log("Nam xuat ban: ", lists[index].year);
+        console.log("Id: ", lists[i].id);
+        console.log("Ten sach: ", lists[i].bookName);
+        console.log("Tac gia: ", lists[i].author);
+        console.log("Nam xuat ban: ", lists[i].year);
         console.log("------------------------")
     }
 };
@@ -39,23 +39,26 @@ while(true) {
     }
     lists.push(list);
     // in danh sach moi
+    console.log(`Đây là danh sách sau khi thêm`)
     bookLists();
 
 } else if(input === 2) {
     //Hien thi danh sach
+        console.log(`Đây là danh sách hiện tại`)
+
     bookLists();
 
 } else if (input===3) {
     // Tim kiem sach theo ten
     let findName = prompt(`Nhap ten sach de tim kiem`);
     let index = lists.findIndex(function(el,i) {
-        return el.name === findName;
+        return el.bookName === findName;
     })
     if (index === -1) {
         console.log(`Sach co ten ${findName} khong ton tai trong danh sach`);
     } else {
-        
-        console.log("Id: ", lists[index].id);
+    console.log(`Đây là thông tin sách bạn tìm`)
+                console.log("Id: ", lists[index].id);
         console.log("Ten sach: ", lists[index].bookName);
         console.log("Tac gia: ", lists[index].author);
         console.log("Nam xuat ban: ", lists[index].year);
@@ -66,19 +69,30 @@ while(true) {
     
 } else if (input === 4) {
     // Xoa sach theo id
-    let deleteBook = prompt(`Nhap ten sach de xoa`);
+    let deleteBook = Number(prompt(`Nhap id sach de xoa`));
     let index = lists.findIndex(function(el,i) {
-        return el.name === deleteBook;
+        return el.id === deleteBook;
     })
      if (index === -1) {
-        console.log(`Sach co ten ${deleteBook} khong ton tai trong danh sach`);
+        console.log(`Sach co id ${deleteBook} khong ton tai trong danh sach`);
     } else {
         lists.splice(index,1);
     
     }
+    console.log(`Đã xóa thành công`)
+    console.log("Id: ", lists[index].id);
+    console.log("Ten sach: ", lists[index].bookName);
+    console.log("Tac gia: ", lists[index].author);
+    console.log("Nam xuat ban: ", lists[index].year);
+    console.log("------------------------")
+    console.log(`Đây là danh sách hiện tại`)
+    bookLists();
+
 
 } else if (input=== 5) {
     // Thoat chuong trinh
+    console.log(`Đã thoát chương trình. Cảm ơn bạn đã sử dụng app`)
+  
     break;
 } else {
    input = Number(prompt(`Lỗi nhập. Bạn vui lòng nhập theo hướng dẫn.
@@ -92,4 +106,16 @@ while(true) {
     Nhập lựa chọn của bạn:`));
     
 }
+ // Cho người dùng nhập lại lựa chọn
+    input = Number(prompt(`
+===== QUẢN LÝ DANH SÁCH SÁCH =====
+
+1. Thêm sách mới
+2. Hiển thị danh sách sách
+3. Tìm kiếm sách theo tên
+4. Xóa sách theo ID
+5. Thoát chương trình
+
+Nhập lựa chọn của bạn:
+`));
 }
